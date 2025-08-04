5afd74929a8aabd2548bfc0588a0076a9f9cdab1
mathpunch Official <coutsozzy09@gmail.com>
2025-08-04 06:21:18 -0500

    Create Settingsapp.jsx

---
+++ src/components/app/Settingsapp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function SettingsApp() {
+  return (
+    <div className="settings-app">
+      <h2>Settings</h2>
+      <p>(Coming soon. Dark theme always on!)</p>
+    </div>
+  );
+}
